50e41fe50ed5a38db6dd47d987f57323d0fcd0e0
nteguem roland <nteguemroland@gmail.com>
2024-07-25 15:41:11 +0100

    review

--- api/services/schedule.service.js
+++ api/services/schedule.service.js
@@ -1,18 +1,22 @@
 const cron = require('node-cron');
 const { fetchAndSaveMatches } = require('./fixture.service');
-const {correctPrediction,publishPrediction} = require("./predict.service");
-const tasks = {}; 
+const { correctPrediction, publishPrediction } = require("./predict.service");
+const tasks = {};
 
-// Obtenir la date d'aujourd'hui au format YYYY-MM-DD
-const today = new Date();
-const todayDate = today.toISOString().split('T')[0];
+// Fonction pour obtenir la date d'aujourd'hui au format YYYY-MM-DD
+function getTodayDate() {
+  const today = new Date();
+  return today.toISOString().split('T')[0];
+}
 
-// Obtenir la date d'hier en utilisant todayDate
-const yesterday = new Date(today);
-yesterday.setDate(today.getDate() - 1); 
-const yesterdayDate = yesterday.toISOString().split('T')[0];
+// Fonction pour obtenir la date d'hier au format YYYY-MM-DD
+function getYesterdayDate() {
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  return yesterday.toISOString().split('T')[0];
+}
 
-// Function to schedule a task
+// Fonction pour planifier une tâche
 function scheduleTask(name, cronTime, taskFunction) {
   tasks[name] = cron.schedule(cronTime, async () => {
     try {
@@ -24,17 +28,17 @@ function scheduleTask(name, cronTime, taskFunction) {
   }, { scheduled: false });
 }
 
-// Function to start a specific task
+// Fonction pour démarrer une tâche spécifique
 function startTask(name) {
   if (tasks[name]) {
     tasks[name].start();
     console.log(`Task "${name}" started.`);
   } else {
     console.log(`Task "${name}" not found.`);
-  } 
-} 
+  }
+}
 
-// Function to stop a specific task
+// Fonction pour arrêter une tâche spécifique
 function stopTask(name) {
   if (tasks[name]) {
     tasks[name].stop();
@@ -44,7 +48,7 @@ function stopTask(name) {
   }
 }
 
-// Function to stop all tasks
+// Fonction pour arrêter toutes les tâches
 function stopAllTasks() {
   Object.keys(tasks).forEach(task => {
     tasks[task].stop();
@@ -52,14 +56,14 @@ function stopAllTasks() {
   });
 }
 
-// Schedule the tasks 
-async function  scheduleAllTasks(client) {
+// Planifier les tâches
+async function scheduleAllTasks(client) {
   await scheduleTask('fetchAndSaveMatches', '49 01 * * *', fetchAndSaveMatches);
   await scheduleTask('correctPrediction', '11 13 * * *', correctPrediction);
-  await scheduleTask('publishPrediction', '12 13 * * *', () => publishPrediction(client, todayDate));
-  await scheduleTask('publishResultPrediction', '32 15 * * *', () => publishPrediction(client, yesterdayDate));
-  await scheduleTask('publishResultPredictionVip', '33 15 * * *', () => publishPrediction(client, yesterdayDate,true));
-  await Object.keys(tasks).forEach(task => startTask(task));
+  await scheduleTask('publishPrediction', '12 13 * * *', () => publishPrediction(client, getTodayDate()));
+  await scheduleTask('publishResultPrediction', '32 15 * * *', () => publishPrediction(client, getYesterdayDate()));
+  await scheduleTask('publishResultPredictionVip', '45 15 * * *', () => publishPrediction(client, getYesterdayDate(), true));
+  Object.keys(tasks).forEach(task => startTask(task));
 }
 
 module.exports = {
