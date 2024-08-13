1378cab332c9429cc2b5b5633f3a74ff48219ec4
nteguem roland <nteguemroland@gmail.com>
2024-08-13 12:47:09 +0100

    review

--- api/helpers/firebase.js
+++ api/helpers/firebase.js
@@ -1,9 +1,9 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./serviceAccountKey.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<your-database-name>.firebaseio.com"
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// //   databaseURL: "https://<your-database-name>.firebaseio.com"
+// });
 
-module.exports = admin;
+// module.exports = admin;
