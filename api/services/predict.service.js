474ec86171824cbc48c1b10a12330add98003cd9
nteguem roland <nteguemroland@gmail.com>
2025-01-18 15:02:49 +0100

    review

--- api/services/predict.service.js
+++ api/services/predict.service.js
@@ -47,15 +47,17 @@ async function deletePrediction(predictionId) {
 
 
 
-async function listPredictions({ offset = 0, limit = 5, date = null, isVisible = null, isVip = false } = {}) {
+async function listPredictions(page = 1, limit = 5, date = null, isVisible = null, isVip = false) {
   try {
     let query = {};
 
     if (date) {
+      // Valider la date
       if (!moment(date).isValid()) {
         throw new Error("Date invalide : " + date);
       }
 
+      // Créer une plage de dates pour couvrir toute la journée
       const startOfDay = moment(date).startOf('day').toISOString();
       const endOfDay = moment(date).endOf('day').toISOString();
 
@@ -65,37 +67,66 @@ async function listPredictions({ offset = 0, limit = 5, date = null, isVisible =
       };
     }
 
+    // Ajouter la condition pour isVisible si elle est passée en paramètre
     if (isVisible !== null) {
       query.isVisible = isVisible;
     }
 
+    // Ajouter la condition pour isVip si elle est passée en paramètre
     if (isVip !== null) {
       query.isVip = isVip;
     }
 
-    const total = await Predict.countDocuments(query);
+    // Si une date est fournie, filtrer par cette date
+    if (date) {
+      const predictions = await Predict.find(query).sort({ "fixture.event_date": -1 });
+      return { success: true, predictions, isFiltered: true };
+    } else {
+      // Obtenir les dates de prédiction distinctes
+      const distinctDates = await Predict.distinct("fixture.event_date");
 
-    const predictions = await Predict
-      .find(query)
-      .sort({ "fixture.event_date": -1 })
-      .skip(offset)
-      .limit(limit);
+      // Filtrer et formater les dates distinctes pour garder uniquement l'année, le mois, et le jour
+      const distinctDatesWithoutTime = distinctDates.map(date => moment(date).format('YYYY-MM-DD'));
 
-    return {
-      success: true,
-      data: {
-        predictions,
-        total,
-        offset,
-        limit
-      }
-    };
+      // Supprimer les doublons
+      const uniqueDates = [...new Set(distinctDatesWithoutTime)];
+
+      // Trier les dates par ordre décroissant (du plus récent au plus ancien)
+      const sortedDates = uniqueDates.sort((a, b) => moment(b).diff(moment(a)));
+
+      // Pagination
+      const totalPages = Math.ceil(uniqueDates.length / limit);
+      const skipCount = (page - 1) * limit;
+
+      // Obtenir les dates pour la page actuelle après pagination
+      const currentDates = sortedDates.slice(skipCount, skipCount + limit);
+
+      // Obtenir les prédictions pour chaque date
+      const groupedPredictions = await Promise.all(
+        currentDates.map(async (date) => {
+          const startOfDay = moment(date).startOf('day').toISOString();
+          const endOfDay = moment(date).endOf('day').toISOString();
+          const predictionsForDate = await Predict.find({
+            "fixture.event_date": {
+              $gte: startOfDay,
+              $lt: endOfDay
+            }
+          });
+          return { date, predictions: predictionsForDate };
+        })
+      );
+
+      return {
+        success: true,
+        totalPages,
+        totalDates: uniqueDates.length,
+        currentPage: page,
+        groupedPredictions
+      };
+    }
   } catch (error) {
-    console.error('Erreur lors de la liste des prédictions:', error);
-    return { 
-      success: false, 
-      error: error.message 
-    };
+    console.log('Erreur lors de la liste des prédictions:', error);
+    return { success: false, error: error.message };
   }
 }
 
