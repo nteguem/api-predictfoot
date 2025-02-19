e8278f04a5d908f778178fbc4cb94579760b4aa2
nteguem roland <nteguemroland@gmail.com>
2025-02-19 10:49:36 +0100

    review

--- app/views/whatsApp/user.js
+++ app/views/whatsApp/user.js
@@ -160,7 +160,7 @@ if (msg.body.startsWith("commande-")) {
     
     try {
       const orderData = JSON.parse(decodedData);
-
+      console.log("orderData",orderData)
       // Structure validation
       if (!orderData || !orderData.plan || !orderData.mobileMoneyPhone) {
         await sendMessageToNumber(client, user.data.phoneNumber,
