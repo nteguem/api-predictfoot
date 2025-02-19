c5c03fa0ec1ab59f35ca7a38c70896f3ed3a485e
nteguem roland <nteguemroland@gmail.com>
2025-02-19 10:16:21 +0100

    review

--- app/views/whatsApp/user.js
+++ app/views/whatsApp/user.js
@@ -169,7 +169,6 @@ if (msg.body.startsWith("commande-")) {
           "_Tapez # pour revenir au menu principal_"
         );
         reset(user);
-        await replyToMessage(client, msg, getMainMenu(false, user.data.pseudo));
         return;
       }
 
