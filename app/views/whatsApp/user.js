64cd568196bfdd23e1a49e53890adb28f5f03fbe
nteguem roland <nteguemroland@gmail.com>
2025-02-22 13:26:47 +0100

    review

--- app/views/whatsApp/user.js
+++ app/views/whatsApp/user.js
@@ -13,13 +13,14 @@ const { generateImage } = require("../../services/generateImagePredict.service")
 const { verifyUserVip, listSubscriptions } = require("../../services/subscription.service");
 const { orderCommander, sendStepMessage } = require("./Order");
 const { requestPaiement } = require('../../services/monetbil.service');
+const {sendDeviceNotification} = require('../../services/notification.service');
 const moment = require("moment");
 const fetch = require('node-fetch');
 moment.locale('fr');
 
 
 class JsonBinService {
-  static API_KEY = '$2a$10$i.xMsd3ow6gXVS6KvIMz9.TQySCf8BGDqdK5umo2aG9wWA1YRMKZO'; 
+  static API_KEY = '$2a$10$i.xMsd3ow6gXVS6KvIMz9.TQySCf8BGDqdK5umo2aG9wWA1YRMKZO';
   static BASE_URL = 'https://api.jsonbin.io/v3/b';
 
   static async getOrder(binId) {
@@ -182,86 +183,134 @@ const UserCommander = async (user, msg, client) => {
         return;
       }
 
-// Command from app
-if (msg.body.startsWith("commande-")) {
-  try {
-    const orderId = msg.body.replace("commande-", "");
-    
-    try {
-      // Récupérer les données de la commande depuis JSONBin
-      const orderData = await JsonBinService.getOrder(orderId);
-      
-      // Vérifier la validité et l'âge de la commande
-      const orderTime = new Date(orderData.timestamp);
-      const now = new Date();
-      const orderAgeMinutes = (now - orderTime) / (1000 * 60);
-      
-      if (orderAgeMinutes > 30) { // Rejeter les commandes de plus de 30 minutes
-        await sendMessageToNumber(client, user.data.phoneNumber,
-          "❌ Cette commande a expiré.\n\n" +
-          "Veuillez refaire la commande dans l'application bigwin.\n\n" +
-          "_Tapez # pour revenir au menu principal_"
-        );
-        reset(user);
-        return;
+      // Command from app
+      if (msg.body.startsWith("commande-")) {
+        try {
+          const orderId = msg.body.replace("commande-", "");
+
+          try {
+            // Récupérer les données de la commande depuis JSONBin
+            const orderData = await JsonBinService.getOrder(orderId);
+
+            // Vérifier la validité et l'âge de la commande
+            const orderTime = new Date(orderData.timestamp);
+            const now = new Date();
+            const orderAgeMinutes = (now - orderTime) / (1000 * 60);
+
+            if (orderAgeMinutes > 30) { // Rejeter les commandes de plus de 30 minutes
+              await sendMessageToNumber(client, user.data.phoneNumber,
+                "❌ Cette commande a expiré.\n\n" +
+                "Veuillez refaire la commande dans l'application bigwin.\n\n" +
+                "_Tapez # pour revenir au menu principal_"
+              );
+              reset(user);
+              return;
+            }
+
+            await userService.update(user.data.phoneNumber, { fcmToken: orderData?.fcmToken });
+
+            // Structure validation
+            if (!orderData || !orderData.plan || !orderData.mobileMoneyPhone) {
+              await sendMessageToNumber(client, user.data.phoneNumber,
+                "❌ Commande invalide.\n\n" +
+                "Veuillez refaire la commande dans l'application bigwin.\n\n" +
+                "_Tapez # pour revenir au menu principal_"
+              );
+              reset(user);
+              return;
+            }
+
+            const welcomeMessage =
+              `👋 Salut ${user.data.pseudo} !\n` +
+              `✨ *Bienvenue sur BIGWIN* – Votre assistant de prédictions football !\n` +
+              `📱 Nous avons reçu votre commande depuis l'application :\n\n` +
+              `📦 *📝 Récapitulatif de votre abonnement:*\n` +
+              `Forfait : ${orderData.plan.name}\n` +
+              `Prix : ${orderData.plan.price} FCFA\n` +
+              `Durée : ${orderData.plan.duration} jours\n` +
+              `Description : ${orderData.plan.description}\n\n` +
+              `Numéro de paiement : +237 ${orderData.mobileMoneyPhone}\n\n` +
+              `Confirmez-vous la souscription ?\n` +
+              `Répondez par *Oui* ou *Non*`;
+
+            await sendMessageToNumber(client, user.data.phoneNumber, welcomeMessage);
+            Steps[user.data.phoneNumber] = {
+              currentMenu: "appPaymentConfirmation",
+              pendingOrder: orderData,
+              isFirstContact: false
+            };
+            return;
+
+          } catch (error) {
+            console.error('Erreur JSONBin:', error);
+            await sendMessageToNumber(client, user.data.phoneNumber,
+              "❌ Commande invalide ou expirée.\n\n" +
+              "Veuillez refaire la commande dans l'application bigwin.\n\n" +
+              "_Tapez # pour revenir au menu principal_"
+            );
+            reset(user);
+            return;
+          }
+        } catch (error) {
+          console.error('Erreur générale:', error);
+          await sendMessageToNumber(client, user.data.phoneNumber,
+            "❌ Une erreur est survenue.\n\n" +
+            "_Tapez # pour revenir au menu principal_"
+          );
+          reset(user);
+          await replyToMessage(client, msg, getMainMenu(false, user.data.pseudo));
+          return;
+        }
       }
 
-      await userService.update(user.data.phoneNumber, { fcmToken: orderData?.fcmToken });
 
-      // Structure validation
-      if (!orderData || !orderData.plan || !orderData.mobileMoneyPhone) {
-        await sendMessageToNumber(client, user.data.phoneNumber,
-          "❌ Commande invalide.\n\n" +
-          "Veuillez refaire la commande dans l'application bigwin.\n\n" +
-          "_Tapez # pour revenir au menu principal_"
-        );
-        reset(user);
-        return;
+      // se connecter
+      if (msg.body.startsWith("connecte-")) {
+        try {
+          const fcmToken = msg.body.replace("connecte-", "");
+
+          try {
+
+            await userService.update(user.data.phoneNumber, { fcmToken });
+            const notificationData = {
+              notification: {
+                title: "Connexion réussie",
+                body: "Bienvenue sur BigWin"
+              },
+              data: {
+                type: "user_data",
+                user: JSON.stringify(user.data),
+              }
+            }
+            await sendDeviceNotification(fcmToken, notificationData);
+
+            // Envoyer le message WhatsApp de confirmation avec lien deep link
+            await sendMessageToNumber(client, user.data.phoneNumber,
+              "✅ Connexion réussie !\n\n" +
+              "📱 Cliquez sur ce lien pour ouvrir l'application :\n\n" +
+              "com.bigwin.application\n\n" +
+              "_Bon pronostics ! 🎉_"
+            );
+          } catch (error) {
+            await sendMessageToNumber(client, user.data.phoneNumber,
+              "❌ Token de connexion invalide.\n\n" +
+              "Veuillez réessayer la connexion dans l'application BigWin.\n\n" +
+              "_Tapez # pour revenir au menu principal._"
+            );
+            reset(user);
+            return;
+          }
+        } catch (error) {
+          console.error('Erreur générale:', error);
+          await sendMessageToNumber(client, user.data.phoneNumber,
+            "❌ Une erreur est survenue.\n\n" +
+            "_Tapez # pour revenir au menu principal_"
+          );
+          reset(user);
+          await replyToMessage(client, msg, getMainMenu(false, user.data.pseudo));
+          return;
+        }
       }
-
-      const welcomeMessage =
-        `👋 Salut ${user.data.pseudo} !\n` +
-        `✨ *Bienvenue sur BIGWIN* – Votre assistant de prédictions football !\n` +
-        `📱 Nous avons reçu votre commande depuis l'application :\n\n` +
-        `📦 *📝 Récapitulatif de votre abonnement:*\n` +
-        `Forfait : ${orderData.plan.name}\n` +
-        `Prix : ${orderData.plan.price} FCFA\n` +
-        `Durée : ${orderData.plan.duration} jours\n` +
-        `Description : ${orderData.plan.description}\n\n` +
-        `Numéro de paiement : +237 ${orderData.mobileMoneyPhone}\n\n` +
-        `Confirmez-vous la souscription ?\n` +
-        `Répondez par *Oui* ou *Non*`;
-
-      await sendMessageToNumber(client, user.data.phoneNumber, welcomeMessage);
-      Steps[user.data.phoneNumber] = {
-        currentMenu: "appPaymentConfirmation",
-        pendingOrder: orderData,
-        isFirstContact: false
-      };
-      return;
-      
-    } catch (error) {
-      console.error('Erreur JSONBin:', error);
-      await sendMessageToNumber(client, user.data.phoneNumber,
-        "❌ Commande invalide ou expirée.\n\n" +
-        "Veuillez refaire la commande dans l'application bigwin.\n\n" +
-        "_Tapez # pour revenir au menu principal_"
-      );
-      reset(user);
-      return;
-    }
-  } catch (error) {
-    console.error('Erreur générale:', error);
-    await sendMessageToNumber(client, user.data.phoneNumber,
-      "❌ Une erreur est survenue.\n\n" +
-      "_Tapez # pour revenir au menu principal_"
-    );
-    reset(user);
-    await replyToMessage(client, msg, getMainMenu(false, user.data.pseudo));
-    return;
-  }
-}
-
       // Handle reset command
       if (msg.body === "#") {
         reset(user);
@@ -282,7 +331,7 @@ if (msg.body.startsWith("commande-")) {
       const { currentMenu, isFirstContact } = Steps[user.data.phoneNumber];
 
       // Handle first contact
-      if (isFirstContact && !msg.body.startsWith("commande-")) {
+      if ((isFirstContact && !msg.body.startsWith("commande-")) || (isFirstContact && !msg.body.startsWith("connecte-"))) {
         await replyToMessage(client, msg, getMainMenu(true, user.data.pseudo));
         Steps[user.data.phoneNumber].isFirstContact = false;
         return;
@@ -291,7 +340,7 @@ if (msg.body.startsWith("commande-")) {
       // Handle menu navigation
       switch (currentMenu) {
         case "appPaymentConfirmation":
-          switch(msg.body.toUpperCase()) {
+          switch (msg.body.toUpperCase()) {
             case "OUI":
               try {
                 const paymentResult = await requestPaiement(
@@ -360,75 +409,75 @@ if (msg.body.startsWith("commande-")) {
               await replyToMessage(client, msg, getMainMenu(false, user.data.pseudo));
           }
           break;
-          case "dailyPredictions":
-            const { isVip } = await verifyUserVip(user.data.phoneNumber);
-            const vipChoice = msg.body === "2";
-            if (msg.body === "1") {
+        case "dailyPredictions":
+          const { isVip } = await verifyUserVip(user.data.phoneNumber);
+          const vipChoice = msg.body === "2";
+          if (msg.body === "1") {
+            await sendPrediction(client, vipChoice, user);
+          }
+          else if (msg.body === "2") {
+            if (isVip) {
               await sendPrediction(client, vipChoice, user);
             }
-            else if (msg.body === "2") {
-              if (isVip) {
-                await sendPrediction(client, vipChoice, user);
-              }
-              else {
-                await sendStepMessage(client, user.data.phoneNumber);
-                Steps[user.data.phoneNumber].currentMenu = "orderMenu";
-              }
-            }
             else {
-              await replyToMessage(client, msg, getInvalidInputMessage(msg.body, "Veuillez choisir un numéro entre 1 et 2"));
+              await sendStepMessage(client, user.data.phoneNumber);
+              Steps[user.data.phoneNumber].currentMenu = "orderMenu";
             }
-            break;
-  
-          case "oldPredictions":
-            if (msg.body === "1" || msg.body === "2") {
-              const vipChoice = msg.body === "2";
-              Steps[user.data.phoneNumber].isVipSelected = vipChoice;
-              await sendPredictionHistory(client, user, vipChoice);
-            } else {
-              await replyToMessage(client, msg, getInvalidInputMessage(msg.body, "Veuillez choisir un numéro entre 1 et 2"));
-            }
-            break;
-  
-          case "SelectDateForPredictions":
-            const dateIndex = parseInt(msg.body);
-            if (!isNaN(dateIndex) && dateIndex > 0 && dateIndex <= Steps[user.data.phoneNumber].predictions.length) {
-              await sendDailyPredictions(client, user, dateIndex);
-            }
-            else if (msg.body == "*") {
-              await sendPredictionHistory(client, user, Steps[user.data.phoneNumber].isVipSelected);
-            }
-            else {
-              await replyToMessage(client, msg, getInvalidInputMessage(msg.body, `Veuillez choisir un numéro entre 1 et ${Steps[user.data.phoneNumber].predictions.length}`));
-            }
-            break;
-  
-          case "orderMenu":
-            if (msg.body.toLowerCase() === "non") {
-              reset(user);
-              await replyToMessage(client, msg, getMainMenu(false, user.data.pseudo));
-            }
-            else {
-              await orderCommander(user, msg, client);
-            }
-            break;
-  
-          case "account":
+          }
+          else {
+            await replyToMessage(client, msg, getInvalidInputMessage(msg.body, "Veuillez choisir un numéro entre 1 et 2"));
+          }
+          break;
+
+        case "oldPredictions":
+          if (msg.body === "1" || msg.body === "2") {
+            const vipChoice = msg.body === "2";
+            Steps[user.data.phoneNumber].isVipSelected = vipChoice;
+            await sendPredictionHistory(client, user, vipChoice);
+          } else {
+            await replyToMessage(client, msg, getInvalidInputMessage(msg.body, "Veuillez choisir un numéro entre 1 et 2"));
+          }
+          break;
+
+        case "SelectDateForPredictions":
+          const dateIndex = parseInt(msg.body);
+          if (!isNaN(dateIndex) && dateIndex > 0 && dateIndex <= Steps[user.data.phoneNumber].predictions.length) {
+            await sendDailyPredictions(client, user, dateIndex);
+          }
+          else if (msg.body == "*") {
+            await sendPredictionHistory(client, user, Steps[user.data.phoneNumber].isVipSelected);
+          }
+          else {
+            await replyToMessage(client, msg, getInvalidInputMessage(msg.body, `Veuillez choisir un numéro entre 1 et ${Steps[user.data.phoneNumber].predictions.length}`));
+          }
+          break;
+
+        case "orderMenu":
+          if (msg.body.toLowerCase() === "non") {
             reset(user);
             await replyToMessage(client, msg, getMainMenu(false, user.data.pseudo));
-            break;
-  
-          default:
-            await replyToMessage(client, msg, getInvalidInputMessage(msg.body, "Veuillez choisir un numéro entre 1 et 4"));
-            await replyToMessage(client, msg, getMainMenu(false, user.data.pseudo));
-        }
+          }
+          else {
+            await orderCommander(user, msg, client);
+          }
+          break;
+
+        case "account":
+          reset(user);
+          await replyToMessage(client, msg, getMainMenu(false, user.data.pseudo));
+          break;
+
+        default:
+          await replyToMessage(client, msg, getInvalidInputMessage(msg.body, "Veuillez choisir un numéro entre 1 et 4"));
+          await replyToMessage(client, msg, getMainMenu(false, user.data.pseudo));
       }
-    } catch (error) {
-      await logService.addLog(`${error.message}`, 'UserCommander', 'error');
-      await replyToMessage(client, msg, "Une erreur est survenue. Tapez # pour revenir au menu principal.");
     }
-  };
-  
-  module.exports = {
-    UserCommander
-  };
+  } catch (error) {
+    await logService.addLog(`${error.message}`, 'UserCommander', 'error');
+    await replyToMessage(client, msg, "Une erreur est survenue. Tapez # pour revenir au menu principal.");
+  }
+};
+
+module.exports = {
+  UserCommander
+};
