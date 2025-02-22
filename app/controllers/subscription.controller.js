7e2f6ba694dcc53981b2717a2a54e9e498faa0e2
nteguem roland <nteguemroland@gmail.com>
2025-02-22 06:14:36 +0100

    review

--- app/controllers/subscription.controller.js
+++ app/controllers/subscription.controller.js
@@ -59,6 +59,7 @@ async function handlePaymentMonetbilSuccess(req, res, client) {
           type: 'subscription_notification',
           subscriptionId: 'operator_transaction_id',
           packageType: 'VIP',
+          user:user,
           startDate: currentDate,
           expiryDate: expire,
           features: [
