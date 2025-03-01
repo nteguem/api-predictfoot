35ce5cc054f8a4fd018c9da50a169615ca7eef5e
nteguem roland <nteguemroland@gmail.com>
2025-03-01 10:00:39 +0100

    review

--- app/routes/index.js
+++ app/routes/index.js
@@ -44,7 +44,7 @@ const setupAppRoutes = (client) => {
   const app = router;
 
   // Apply the global middleware to all routes with specified exclusions
-  app.use(globalAuthenticate(['/user/login', '/predict/list', '/subscription/notification-payment','/plan/list','/subscription/is-vip','/user/getOne',':bot/info','/bot/disconnect','/bot/reconnect']));
+  app.use(globalAuthenticate(['/user/login', '/predict/list', '/subscription/notification-payment','/plan/list','/subscription/is-vip','/user/getOne','/bot/info','/bot/disconnect','/bot/reconnect']));
 
   setupUserRoutes(app, client);
   setupGroup(app, client);
