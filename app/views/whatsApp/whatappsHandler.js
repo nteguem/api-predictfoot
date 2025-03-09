aff106c1f87fe011a8d5d6ab843affce13663f5b
nteguem roland <nteguemroland@gmail.com>
2025-03-09 13:19:52 +0100

    review

--- app/views/whatsApp/whatappsHandler.js
+++ app/views/whatsApp/whatappsHandler.js
@@ -125,7 +125,8 @@ const handleMessage = async (client, message) => {
   try {
     const senderJid = message.key.remoteJid;
     const messageContent = message.message;
-    
+    console.log("roland0000",message)
+
     // Extract sender's number from JID (remove "@s.whatsapp.net")
     const senderNumber = senderJid.split('@')[0];
     
@@ -135,7 +136,6 @@ const handleMessage = async (client, message) => {
     const response = await save(senderNumber, senderName);
     
     if (response?.data?.role === "user") {
-      console.log("roland",response)
       await UserCommander(response, { 
         from: senderJid, 
         body: messageContent?.conversation || messageContent?.extendedTextMessage?.text || '', 
