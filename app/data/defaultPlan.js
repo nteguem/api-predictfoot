c49f4afb6c064776b45a0a4d576a70acc157158a
nteguem roland <nteguemroland@gmail.com>
2025-03-19 10:15:27 +0100

    review

--- app/data/defaultPlan.js
+++ app/data/defaultPlan.js
@@ -2,9 +2,6 @@ const defaultPlans = [
     // { name: 'Forfait Gratuit', price: 0, duration: 0, description: 'Accès illimité aux pronostics gratuits.' },
     { name: 'Découverte', price: 500, duration: 2, description: 'Accès aux pronostics pour 2 jours.' },
     { name: 'Hebdomadaire', price: 1500, duration: 7, description: 'Accès aux pronostics pour 7 jours.' },
-    { name: 'Mensuel', price: 5000, duration: 30, description: 'Accès aux pronostics pour 30 jours.' },
-    { name: 'Trimestriel', price: 13000, duration: 90, description: 'Accès aux pronostics pour 90 jours.' },
-    { name: 'Semestriel', price: 24000, duration: 180, description: 'Accès aux pronostics pour 180 jours.' }
-];
+    { name: 'Mensuel', price: 5000, duration: 30, description: 'Accès aux pronostics pour 30 jours.' },];
 
 module.exports = {defaultPlans};
