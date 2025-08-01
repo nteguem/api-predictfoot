8f91f1e61fe0227b770947d1af9efd419157acc9
nteguem roland <nteguemroland@gmail.com>
2025-08-01 05:02:42 +0100

    review

--- app/services/schedule.service.js
+++ app/services/schedule.service.js
@@ -65,8 +65,8 @@ function stopAllTasks() {
 
 // Planifier les tâches
 async function scheduleAllTasks(client) {
-  await scheduleTask('fetchAndSaveMatches', '57 08 * * *', fetchAndSaveMatches);
-  await scheduleTask('correctPrediction', '57 08 * * *', correctPrediction);
+  await scheduleTask('fetchAndSaveMatches', '32 02 * * *', fetchAndSaveMatches);
+  await scheduleTask('correctPrediction', '33 02 * * *', correctPrediction);
   await scheduleTask('publishAllPredictions', '30 10 * * *', async () => {
     await publishPrediction(client, getYesterdayDate());  // Envoi des résultats de la veille
     await publishPrediction(client, getTodayDate());      // Envoi des prédictions du jour
